46a8e0ff340ecec1a7d94f27ebe154b86d44138e
Chirag Sane <chiragsane333@gmail.com>
2020-03-22 01:49:03 +0530

    implement fullpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'v9app';
+  config: any;
+  fullpageAPI: any;
+
+  isFirst = true;
+
+  constructor() {
+
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['', '', '', '', ''],
+      menu: '#menu',
+      navigation: true,
+      onLeave: (origin: any, destination: any, direction: any) => {
+        this.isFirst = destination.isFirst;
+      }
+    };
+  }
+
+  getRef(fullPageRef: any) {
+    this.fullpageAPI = fullPageRef;
+  }
 }
